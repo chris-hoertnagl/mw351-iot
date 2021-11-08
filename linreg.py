da6df55642b3817b85434f46135324001a995893
Chris <christophhoertnagl@gmail.com>
2021-11-08 21:39:33 +0100

    finalize linreg

--- linreg.py
+++ linreg.py
@@ -2,12 +2,6 @@ import numpy as np
 import pandas as pd
 from sklearn import linear_model
 
-data = [("2021-11-08 21:31:53" ,10691.50), ("2021-11-08 21:31:53" ,10691.50), ("2021-11-06 15:00:53",  6000.3), ("2021-11-06 16:00:53",  6000.3), ("2021-11-05 01:31:00",  789.32)]
-df = pd.DataFrame(data)
-df.columns = ["date", "power"]
-df.date = pd.to_datetime(df.date)
-df
-
 def predict(df):
     df.sort_values("date", ascending=True, inplace=True)
     df['days_from_start'] = (df.date - df.date.values[0]).dt.days
@@ -19,17 +13,14 @@ def predict(df):
     model.fit(X, y)
 
     today = df.days_from_start.max()
+    power_cons_today = df.loc[df.days_from_start == today, 'power'].values[0]
     prediction_day = today + 30
     prediciton = model.predict([[prediction_day]])
-    print(today)
-    print(prediction_day)
-    print(prediciton[0])
-    print(df.loc[df.days_from_start == today, "power"].values[0])
+    
+    print(f"consumption today: {power_cons_today}")
+    print(f"consumption in 30 days: {prediciton[0]}")
 
-    estimation = prediciton[0] - df.loc[df.days_from_start == today, "power"].values[0]
-    print(estimation)
+    estimation = prediciton[0] - power_cons_today
 
     print(f"Estimated Energy consumption for the next 30 days: {estimation}")
     return estimation
-
-predict(df)
