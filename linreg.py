75858ba52020b83529bf8dfed84318564b05abb7
Chris <christophhoertnagl@gmail.com>
2021-11-08 21:26:00 +0100

    update linreg

--- linreg.py
+++ linreg.py
@@ -15,7 +15,10 @@ def predict(df):
     today = df.days_from_start.max()
     prediction_day = today + 30
     prediciton = model.predict([[prediction_day]])
-    estimation = prediciton[0] - df.loc[df.days_from_start == today, "power"][0]
+    print(today)
+    print(prediction_day)
+    print(prediciton)
+    #estimation = prediciton[0] - df.loc[df.days_from_start == today, "power"][0]
 
-    print(f"Estimated Energy consumption for the next 30 days: {estimation}")
-    return estimation
+    #print(f"Estimated Energy consumption for the next 30 days: {estimation}")
+    #return estimation
