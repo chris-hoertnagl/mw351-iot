82e631f7366f18906f0b4e724dc3843751ae0566
Chris <christophhoertnagl@gmail.com>
2021-11-08 21:30:28 +0100

    update linreg again

--- linreg.py
+++ linreg.py
@@ -2,6 +2,12 @@ import numpy as np
 import pandas as pd
 from sklearn import linear_model
 
+data = [("2021-11-08 21:31:53" ,10691.50), ("2021-11-08 21:31:53" ,10691.50), ("2021-11-06 15:00:53",  6000.3), ("2021-11-06 16:00:53",  6000.3), ("2021-11-05 01:31:00",  789.32)]
+df = pd.DataFrame(data)
+df.columns = ["date", "power"]
+df.date = pd.to_datetime(df.date)
+df
+
 def predict(df):
     df.sort_values("date", ascending=True, inplace=True)
     df['days_from_start'] = (df.date - df.date.values[0]).dt.days
@@ -18,7 +24,12 @@ def predict(df):
     print(today)
     print(prediction_day)
     print(prediciton)
-    #estimation = prediciton[0] - df.loc[df.days_from_start == today, "power"][0]
+    print(df.loc[df.days_from_start == today, "power"][0])
 
-    #print(f"Estimated Energy consumption for the next 30 days: {estimation}")
-    #return estimation
+    estimation = prediciton - df.loc[df.days_from_start == today, "power"]
+    print(estimation)
+
+    print(f"Estimated Energy consumption for the next 30 days: {estimation}")
+    return estimation
+
+predict(df)
