2a620a4890693f82933fe17bde0e142c00d7b812
Chris <christophhoertnagl@gmail.com>
2021-11-21 19:29:30 +0100

    update test to get csv

--- test.py
+++ test.py
@@ -1,5 +1,25 @@
 import data_handler
+import psycopg2
+import pandas as pd
+
+# Connect to an existing database
+connection = psycopg2.connect(user="pi",
+                              password="pw_raspberry",
+                              host="127.0.0.1",
+                              port="5432",
+                              database="iotdb")
+try:
+    # Create a cursor to perform database operations
+    cursor = connection.cursor()
+    cursor.execute("SELECT * from ENERGYMGMT")
+    df = pd.DataFrame(cursor.fetchall())
+    print("Created df from database")
+    df.columns = ["date", "power"]
+    df.date = pd.to_datetime(df.date)
+    df.power = df.power.astype(float)
+    df.to_csv("data.csv")
+except (Exception, psycopg2.Error) as error:
+    print("Error while connecting to PostgreSQL", error)
 
 dh = data_handler.DataHandler()
 print(dh.predict())
-
